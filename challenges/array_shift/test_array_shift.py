1d3ef8d2cdec88b170bd95b3dc025153a1ad9545
JCode1986 <joseph.hangarter@yahoo.com>
2019-12-03 14:07:17 -0800

    completed functions for challenge

---
+++ challenges/array_shift/test_array_shift.py
@@ -0,0 +1,7 @@
+from array_shift import insert_shift_array
+
+
+# def test_one():       //test needs to be in function name and file
+#   expected = 1
+#   actual = some_function_here(1)
+#   assert actual == expected
