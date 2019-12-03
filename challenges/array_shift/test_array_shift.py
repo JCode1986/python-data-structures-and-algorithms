a1fce8b3ac903b08d2cd3caa5ab29bbb5a9bf68a
JCode1986 <joseph.hangarter@yahoo.com>
2019-12-03 14:45:03 -0800

    pytest added, and tests in module

--- challenges/array_shift/test_array_shift.py
+++ challenges/array_shift/test_array_shift.py
@@ -1,7 +1,21 @@
 from array_shift import insert_shift_array
+import pytest
 
+array_one = [1, 2, 3, 5, 6]
+array_two = [7, 8, 9, 11, 12, 13]
 
-# def test_one():       //test needs to be in function name and file
-#   expected = 1
-#   actual = some_function_here(1)
-#   assert actual == expected
+#odd list
+def test_one():       
+  expected = [1, 2, 3, 4, 5, 6]
+  actual = insert_shift_array(array_one, 4)
+  assert actual == expected
+
+#even list
+def test_two():       
+  expected = [7, 8, 9, 10, 11, 12, 13]
+  actual = insert_shift_array(array_two, 10)
+  assert actual == expected
+
+def test_three():
+  with pytest.raises(TypeError):  # Pass in the expected error
+    insert_shift_array('String', 'x')
