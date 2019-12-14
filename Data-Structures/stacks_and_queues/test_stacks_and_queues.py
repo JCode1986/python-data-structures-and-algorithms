c04e1687e97f8cbe63679d9f4e5fe4615042c740
Joseph Hangarter <joseph.hangarter@yahoo.com>
2019-12-13 17:17:37 -0800

    added some tests

--- Data-Structures/stacks_and_queues/test_stacks_and_queues.py
+++ Data-Structures/stacks_and_queues/test_stacks_and_queues.py
@@ -1,2 +1,77 @@
 import pytest
 from stacks_and_queues import Node, Stack, Queue
+
+def test_one():
+    """Can successfully push onto a stack"""
+    stack = Stack()
+    assert stack
+
+def test_one():
+    """Can successfully push multiple values onto a stack"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully push multiple values onto a stack"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully pop off the stack"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully empty a stack after multiple pops"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully peek the next item on the stack"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully instantiate an empty stack"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully enqueue into a queue"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully enqueue multiple values into a queue"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully dequeue out of a queue the expected value"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully peek into a queue, seeing the expected value"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully empty a queue after multiple dequeues"""
+    queue = Queue()
+    assert queue
+
+def test_one():
+    """Can successfully instantiate an empty queue"""
+    queue = Queue()
+    assert queue
+
+
+
+# def test_two():
+#     """Can properly insert into the Queue"""
+#     expected = 2
+#     linked_list.insert(1)
+#     linked_list.insert(2)
+#     actual = linked_list.head.value
+#     assert actual == expected
