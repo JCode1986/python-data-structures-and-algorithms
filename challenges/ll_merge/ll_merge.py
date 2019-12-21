4ac662767116c0c2bad1fc23620bc280f7100976
Joseph Hangarter <joseph.hangarter@yahoo.com>
2019-12-20 22:00:43 -0800

    added testing to ll-merge

--- challenges/ll_merge/ll_merge.py
+++ challenges/ll_merge/ll_merge.py
@@ -1,4 +1,4 @@
-from 
+from linked_list import Node, Linked_list
 
 def merge_lists(list1, list2): 
 
@@ -24,3 +24,25 @@ def merge_lists(list1, list2):
         b_curr = b_ref
 
     return a_curr 
+
+if __name__ == "__main__":
+    lst1 = Linked_list()
+    lst2 = Linked_list()
+    lst3 = Linked_list()
+    lst1.insert(1)
+    lst1.append(2)
+    lst1.append(3)
+    lst1.append(2)
+    lst1.append(1)
+    lst2.insert(2)
+    lst2.append(4)
+    lst2.append(6)
+    lst2.append(8)
+    lst2.append(10)
+    lst3.insert(420)
+    lst3.append(69)
+    print(lst1.to_string())
+    print(lst1.to_string())
+    merge_lists(lst1, lst2)
+    print(lst1.to_string())
+    # print(lst3.to_string())
--- challenges/ll_merge/test_merge.py
+++ challenges/ll_merge/test_merge.py
@@ -1,3 +1,30 @@
-from ll_merge import merge_lists
+from ll_merge import merge_lists, Node, Linked_list
 import pytest
 
+@pytest.fixture
+def list1():
+    list1 = Linked_list()
+    l1st1.insert(1)
+    array = [3, 5, 7, 9]
+    for num in array:
+        list1.append(num)
+    # l1st1.append(3)
+    # l1st1.append(5)
+    # l1st1.append(7)
+    # l1st1.append(9)
+    return list1
+
+@pytest.fixture
+def list2():
+    list2 = Linked_list()
+    list2.insert(2)
+    list2.append(4)
+    list2.append(6)
+    list2.append(8)
+    list2.append(10)
+    return list2
+
+def test_merge_lists(list1, list2):
+    actual = merge_lists(list1, list2)
+    expected = 0
+    assert actual == expected 
