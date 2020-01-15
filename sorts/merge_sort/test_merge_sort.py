dc0408e7528f38766ac92c27ecb9f8c00bb3f64b
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-01-14 22:27:21 -0800

    completed merge_sort

--- sorts/merge_sort/test_merge_sort.py
+++ sorts/merge_sort/test_merge_sort.py
@@ -1,7 +1,7 @@
 from merge_sort import merge_sort, merge_sort
 import pytest
 
-@pytest.mark.skip()
+
 def test_merge_sort_list_unique():
     """Can sort list utilizing insertion sort method"""
     nums = [23, 1, 5, 69, 808, 420, 33, 1986]
@@ -9,7 +9,7 @@ def test_merge_sort_list_unique():
     expected = [1, 5, 23, 33, 69, 420, 808, 1986]
     assert actual == expected
 
-@pytest.mark.skip()
+
 def test_merge_sort_list_negative():
     """Can sort list utilizing insertion sort method"""
     nums = [23, 1, 5, -69, 808, -420, 33, 1986]
@@ -17,7 +17,7 @@ def test_merge_sort_list_negative():
     expected = [-420, -69, 1, 5, 23, 33, 808, 1986]
     assert actual == expected
 
-@pytest.mark.skip()
+
 def test_merge_sort_list_duplicates():
     """Can sort list utilizing insertion sort method"""
     nums = [23, 1, 5, -69, 808, 808, -420, 33, 33, 1986]
@@ -25,10 +25,3 @@ def test_merge_sort_list_duplicates():
     expected = [-420, -69, 1, 5, 23, 33, 33, 808, 808, 1986]
     assert actual == expected
 
-@pytest.mark.skip()
-def test_merge_sort_list_string():
-    """Can sort list utilizing insertion sort method"""
-    nums = [23, 1, '5', -69, 808, -420, '33', 1986]
-    actual = merge_sort(nums)
-    expected = [-420, -69, 1, 5, 23, 33, 808, 1986]
-    assert actual == expected
