a3b88f2bb3515903166619441fd907e67842ba3f
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-01-20 11:58:19 -0800

    completed functions, just need image

--- challenges/repeated_word/repeated_word.py
+++ challenges/repeated_word/repeated_word.py
@@ -1,14 +1,48 @@
-def repeated_word(string):
+def first_repeated_word(string):
     """
     Function that returns that first repeated word in a string
     In: String
     out: String
     """
+
     word_split = string.lower().split(' ')
     word_list = []
+
     for word in word_split:
         if word in word_list:
             return word
         word_list.append(word)
     
     return None
+
+def most_repeated_word(string):
+    """
+    Function that returns that most repeated word in a string
+    In: String
+    out: String
+    """
+
+    word_list = string.lower().split(' ')
+    dictionary = {}
+    most_repeated_value = 0
+    most_repeated_key = ''
+
+    i = 0
+    for word in word_list:
+        if word_list[i] in dictionary:
+            dictionary[word_list[i]] += 1
+        
+        else:
+            dictionary[word_list[i]] = 1
+        i += 1
+
+    j = 0
+    for key in dictionary:
+        current_val = dictionary.get(key)
+        if current_val:
+            if current_val > most_repeated_value:
+                most_repeated_value = current_val
+                most_repeated_key = key
+        j += 1
+    
+    return most_repeated_key
--- challenges/repeated_word/test_repeated_word.py
+++ challenges/repeated_word/test_repeated_word.py
@@ -1,14 +1,26 @@
-from repeated_word import repeated_word
+from repeated_word import first_repeated_word, most_repeated_word
 import pytest
 
 def test_returns_first_repeated_word():
     sentence = 'In a galaxy far far away'
-    actual = repeated_word(sentence)
+    actual = first_repeated_word(sentence)
     expected = 'far'
     assert actual == expected
 
 def test_more_than_one_repeated_word_in_string():
     sentence = 'The word the is the first repeated word in this sentence. Testing: 1 1 1 1 1 1'
-    actual = repeated_word(sentence)
+    actual = first_repeated_word(sentence)
     expected = 'the'
     assert actual == expected
+
+def test_lower_or_uppercase_does_not_matter():
+    sentence = 'It does not matter. -- it --'
+    actual = first_repeated_word(sentence)
+    expected = 'it'
+    assert actual == expected
+
+def test_returns_most_repeated_word():
+    sentence = 'In a galaxy far far away. In a galaxy even further away. Hey hey hey hey hey.'  
+    actual = most_repeated_word(sentence)
+    expected = 'hey'
+    assert actual == expected      
